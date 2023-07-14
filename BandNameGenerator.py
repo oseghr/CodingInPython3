32961455b4c23f1f9d2e397cfabe5d1e1dc9e7e1
Ose Benson <o.oaikhena@yahoo.com>
2023-07-13 22:53:59 -0600

    Day1: Band Name Generator

---
+++ BandNameGenerator.py
@@ -0,0 +1,24 @@
+"""
+Day 1: Printing outputs, Variables, Getting User Inputs, Strings, Strings Concatenation
+
+"""
+
+#Band Name Generator
+
+#Display our application name
+print("Welcome to Band Name Generator")
+
+#display our first hint question
+print("What is the name of the city you grew up in?")
+
+#stores the first input from user
+city = input()
+
+#display our first hint question
+print("what's your pet's name?")
+
+#stores the second input from the user
+pet = input()
+
+#displays the band name generated.
+print("Your Band Name could be:", city, pet)
